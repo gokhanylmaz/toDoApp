c702e886192625d52f42b76f913588020230b9c1
Gökhan Yılmaz <91563627+gokhanylmaz@users.noreply.github.com>
2022-12-11 23:34:56 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { useState } from 'react';
 
 function App() {
+
+  const [text, setText] = useState()
+  const [message, setMessage] = useState([])
+  console.log('text', text);
+  const onChangeFunc = (e) => {
+    setText(e.target.value)
+  }
+  const messageFunc = () => {
+    setMessage(prev => [...prev, text])
+    setText('')
+  }
+  console.log(message)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='todoApp text-center mt-5'>
+        <h1 className='mb-5 display-3'>To Do App </h1>
+        <div className='d-flex justify-content-center mb-5'>
+          <input value={text} onChange={onChangeFunc} type="text" placeholder='add' />
+          <button onClick={messageFunc} className='btn btn-dark'>Add</button>
+        </div>
+        {
+          message?.map((msg,i)=>(
+            <div className='p-2 border' key={i}>{msg}</div>
+          ))
+        }
     </div>
   );
 }
